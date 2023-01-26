b6880d037cc21a9587a0b9c513a6a35876a4420a
Johan Ejstrud <johan@ejstrud.com>
2023-01-26 13:34:56 -0300

    Modify to pass devtools::check() without errors

--- R/globals.R
+++ R/globals.R
@@ -21,14 +21,14 @@ r_dir <- file.path('R')
 helper_functions_file_path <- file.path(r_dir, 'helper_functions.R')
 
 get_excel_metadata_path <- function(table_name) {
-  file.path(metadata_dir, str_glue("metadata_{str_to_upper(table_name)}.xlsx"))
+  file.path(metadata_dir, stringr::str_glue("metadata_{stringr::str_to_upper(table_name)}.xlsx"))
 }
 
 get_source_data_path <- function(table_name) {
-  file.path(data_raw_dir, str_glue("{str_to_upper(table_name)}.rds"))
+  file.path(data_raw_dir, stringr::str_glue("{stringr::str_to_upper(table_name)}.rds"))
 }
 
 get_px_file_path <- function(table_name) {
-  file.path(data_dir, str_glue("{str_to_upper(table_name)}.px"))
+  file.path(data_dir, stringr::str_glue("{stringr::str_to_upper(table_name)}.px"))
 }
 
--- tests/testthat/test-helper_functions.R
+++ tests/testthat/test-helper_functions.R
@@ -1,15 +1,12 @@
-project_dir <- file.path('..', '..')
-source(file.path(project_dir, 'R', 'helper_functions.R'))
-
 test_that("strings are quoted", {
   input  <- c('hi',  ' " ',    'single\'quote')
   expect <- c('"hi"', '" " "', '"single\'quote"')
-  
+
   expect_equal(str_quote(input), expect)
 })
 
 test_that("keywords has language", {
-  expect_equal(add_language_to_keyword(keyword = rep('STUB', 4), 
+  expect_equal(add_language_to_keyword(keyword = rep('STUB', 4),
                                        language = c("en", "da", "kl", NA)
                                        ),
                c("STUB", "STUB[da]", "STUB[kl]", "STUB")
@@ -19,10 +16,10 @@ test_that("keywords has language", {
 
 test_that("keyword has sub-key", {
   expect_equal(add_sub_key_to_keyword(keyword = c("ELIMINATION",
-                                                "ELIMINATION[da]", 
-                                                "ELIMINATION[kl]"), 
+                                                "ELIMINATION[da]",
+                                                "ELIMINATION[kl]"),
                                       name    = c("place of birth",
-                                                "fødested", 
+                                                "fødested",
                                                 "inunngorfik"
                                                 )
                                       ),
@@ -37,5 +34,5 @@ test_that("strings are quoted", {
   input  <- c(  'A',   ' b ',   '"', 0, 3.14, 'YES', 'NO', '"is quoted"')
   expect <- c('"A"', '" b "', '"""', 0, 3.14, 'YES', 'NO', '"is quoted"')
 
-  expect_equal(quote_unless_numeric_or_yes_no(input), expect)  
+  expect_equal(quote_unless_numeric_or_yes_no(input), expect)
 })

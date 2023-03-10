195545a4b58d8a4dc245441578dc230460cd6343
Johan Ejstrud <johan@ejstrud.com>
2023-03-10 14:07:59 -0300

    Add test for all px files

--- tests/testthat/test-metamake.R
+++ tests/testthat/test-metamake.R
@@ -1,14 +1,20 @@
 test_that("metamake is the inverse of pxmake", {
-  px_source    <- get_pxfile_path("bexsta")
-  metadata_out <- get_metadata_path("bexsta_by_metamake")
-  px_out       <- get_pxfile_path("BEXSTA_clone")
+  run_metmake_pxmake_and_compare <- function(table_name) {
+    px_source    <- get_pxfile_path(table_name)
+    metadata_out <- get_metadata_path(paste0(table_name, "_by_metamake"))
+    px_out       <- get_pxfile_path(paste0(table_name, "_metamake_pxmake"))
 
-  metamake(px_source, metadata_out)
+    metamake(px_source, metadata_out)
 
-  pxmake(metadata_out, px_out)
+    pxmake(metadata_out, px_out)
 
-  output <- readLines(px_source)
-  expect <- readLines(px_out)
+    output <- readLines(px_source)
+    expect <- readLines(px_out)
 
-  expect_equal(output, expect)
+    expect_equal(output, expect)
+  }
+
+  run_metmake_pxmake_and_compare("BEXLTALL")
+  run_metmake_pxmake_and_compare("BEXSTA")
+  run_metmake_pxmake_and_compare("FOTEST")
 })

c550d583d83e95f41d480ecbb93a14b357f6568f
Johan Ejstrud <johan@ejstrud.com>
2023-03-14 09:51:06 -0300

    Manually check that equivalence test passes

--- tests/testthat/test-metamake.R
+++ tests/testthat/test-metamake.R
@@ -1,15 +1,19 @@
+run_metamake_and_pxmake <- function(table_name) {
+  px_source    <- get_pxfile_path(table_name)
+  metadata_out <- get_metadata_path(paste0(table_name, "_by_metamake"))
+  px_out       <- get_pxfile_path(paste0(table_name, "_metamake_pxmake"))
+
+  metamake(px_source, metadata_out)
+
+  pxmake(metadata_out, px_out)
+}
+
 test_that("pxfile = pxmake(metamake(pxfile))", {
   run_metamake_pxmake_and_compare <- function(table_name) {
-    px_source    <- get_pxfile_path(table_name)
-    metadata_out <- get_metadata_path(paste0(table_name, "_by_metamake"))
-    px_out       <- get_pxfile_path(paste0(table_name, "_metamake_pxmake"))
+    run_metamake_and_pxmake(table_name)
 
-    metamake(px_source, metadata_out)
-
-    pxmake(metadata_out, px_out)
-
-    output <- readLines(px_source)
-    expect <- readLines(px_out)
+    output <- readLines(get_pxfile_path(table_name))
+    expect <- readLines(get_pxfile_path(paste0(table_name, "_metamake_pxmake")))
 
     expect_equal(output, expect)
   }
@@ -25,12 +29,24 @@ test_that("pxfile and pxmake(metamake(pxfile)) are equivalent", {
   # that they are equivalent.
   skip_if_not_installed("pxjob64Win", minimum_version = "1.1.0")
 
-  pxjob_runs_without_erros <- function(table_name) {
-    0 == pxjob64Win::pxjob(input = get_pxfile_path(table_name),
-                           output = get_pxjobfile_path(table_name)
+  skip(TRUE) # Turn on when support for CELLNOTEX is added (issue #101)
+
+  run_metamake_pxmake_pxjob_and_compare <- function(table_name) {
+    run_metamake_and_pxmake(table_name)
+
+    pxjob64Win::pxjob(input = get_pxfile_path(table_name),
+                      output = get_pxjobfile_path(table_name)
     )
+
+    pxjob64Win::pxjob(input = get_pxfile_path(paste0(table_name, "_metamake_pxmake")),
+                      output = get_pxjobfile_path(paste0(table_name, "_metamake_pxmake"))
+    )
+
+    output <- readLines(get_pxjobfile_path(table_name))
+    expect <- readLines(get_pxjobfile_path(paste0(table_name, "_metamake_pxmake")))
+
+    expect_equal(output, expect)
   }
 
-
-  #run_metamake_pxmake_and_compare("TUX01")
+  run_metamake_pxmake_pxjob_and_compare("TUX01")
 })

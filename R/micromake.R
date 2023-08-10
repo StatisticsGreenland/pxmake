7b4b248c1dcb0f13f92ccc287fa7b7d30b0e0987
Johan Ejstrud <johan@ejstrud.com>
2023-08-10 11:57:56 +0200

    Add more functionality

--- R/micromake.R
+++ R/micromake.R
@@ -15,47 +15,92 @@ micromake <- function(input, data) {
   # Create rds file for each variable
 
 
+  get_one_var('gender')
+  get_one_var('place of birth')
+}
+
+get_one_var <- function(varname) {
   rds <- pxmake(get_metadata_path('bexsta'), data = get_data_path('bexsta'))
 
-  get_one_var <- function(varname) {
-    name_relation <-
-      rds$metadata %>%
-      dplyr::filter(keyword == "VARIABLECODE") %>%
-      tidyr::unnest(value) %>%
-      dplyr::select(`variable-code` = value, variable, language)
+  name_relation <-
+    rds$metadata %>%
+    dplyr::filter(keyword == "VARIABLECODE") %>%
+    tidyr::unnest(value) %>%
+    dplyr::select(`variable-code` = value, variable, language)
 
-    vars <-
-      dplyr::distinct(name_relation, `variable-code`) %>%
-      dplyr::pull(`variable-code`)
+  vars <-
+    dplyr::distinct(name_relation, `variable-code`) %>%
+    dplyr::pull(`variable-code`)
 
-    metadata <-
-      rds$metadata %>%
-      dplyr::left_join(name_relation, by = c('variable', 'language'))
+  metadata <-
+    rds$metadata %>%
+    dplyr::left_join(name_relation, by = c('variable', 'language'))
 
-    head_and_stub <-
-      rds$metadata %>%
-      dplyr::filter(keyword %in% c("STUB", "HEADING")) %>%
-      tidyr::unnest(value) %>%
-      dplyr::left_join(name_relation, by = c('value' = 'variable', 'language')) %>%
-      dplyr::distinct(`variable-code`) %>%
-      dplyr::pull(`variable-code`)
+  time_var <-
+    metadata %>%
+    dplyr::filter(keyword == "TIMEVAL") %>%
+    dplyr::distinct(`variable-code`) %>%
+    dplyr::pull(`variable-code`)
 
-    figures_var <- setdiff(vars, head_and_stub)
+  head_and_stub <-
+    rds$metadata %>%
+    dplyr::filter(keyword %in% c("STUB", "HEADING")) %>%
+    tidyr::unnest(value) %>%
+    dplyr::left_join(name_relation, by = c('value' = 'variable', 'language')) %>%
+    dplyr::distinct(`variable-code`) %>%
+    dplyr::pull(`variable-code`)
 
-    # also include timevar
+  figures_var <- setdiff(vars, head_and_stub)
 
-    # loop
-    metadata_new <-
-      metadata %>%
-      dplyr::filter(is.na(`variable-code`) | `variable-code` == varname)
+  # also include timevar
 
-    data_new <-
-      rds$data %>%
-      dplyr::select(all_of(c(varname, figures_var)))
+  include_vars <- c(varname, figures_var, time_var)
 
-    return(list('metadata' = metadata_new, 'data' = data_new))
-  }
+  # loop
+  new_head_stub <-
+    metadata %>%
+    dplyr::filter(keyword %in% c("STUB", "HEADING")) %>%
+    dplyr::select(-`variable-code`) %>%
+    tidyr::unnest(value) %>%
+    dplyr::left_join(name_relation, by = c('value' = 'variable', 'language')) %>%
+    dplyr::filter(`variable-code` %in% include_vars) %>%
+    dplyr::select(-`variable-code`) %>%
+    dplyr::group_by(across(-value)) %>%
+    dplyr::summarise(value = list(value), .groups = "keep")
 
 
+  #use variable and stub, and timevar as heading
+  stub_df <-
+    name_relation %>%
+    dplyr::filter(`variable-code` %in% varname) %>%
+    dplyr::mutate(keyword = "STUB") %>%
+    dplyr::rename(value = variable) %>%
+    wrap_varaible_in_list(value)
 
+  heading_df <-
+    name_relation %>%
+    dplyr::filter(`variable-code` %in% time_var) %>%
+    dplyr::mutate(keyword = "HEADING") %>%
+    dplyr::rename(value = variable) %>%
+    wrap_varaible_in_list(value)
+
+  metadata_new <-
+    metadata %>%
+    dplyr::filter(is.na(`variable-code`) | `variable-code` %in% include_vars) %>%
+    dplyr::filter(!keyword %in% c("STUB", "HEADING")) %>%
+    dplyr::bind_rows(stub_df,
+                     heading_df
+                     )
+
+  data_new <-
+    rds$data %>%
+    dplyr::as_tibble() %>%
+    dplyr::filter(dplyr::if_all(everything(), ~ . != 'T')) %>% #elimination
+    dplyr::select(all_of(include_vars)) %>%
+    dplyr::group_by(across(-all_of(figures_var))) %>%
+    dplyr::summarise({{figures_var}} := sum(!!rlang::sym(figures_var), na.rm = TRUE))
+
+  rds_new <- list('metadata' = metadata_new, 'data' = data_new)
+
+  pxmake(rds_new, paste0(varname, '.px'))
 }

2dcc42c741fe032da34fdee5d7fa0e060ecedb97
johan-ejstrud <johan@ejstrud.com>
2024-11-28 08:03:37 -0200

    Support cell values that contain closing parentheses
    Support cell values that contain closing parentheses
    
    Close #337

--- tests/testthat/test-60-px-pxsave-preserves.R
+++ tests/testthat/test-60-px-pxsave-preserves.R
@@ -1,7 +1,30 @@
 test_that("px file = px_save(px(px file))", {
-  expect_px_px_save_preserves_everything("BEXSTA")
-  expect_px_px_save_preserves_everything("FOTEST")
-  expect_px_px_save_preserves_everything("no_timeval_or_codes")
-  expect_px_px_save_preserves_everything("zero_heading")
-  expect_px_px_save_preserves_everything("zero_stub")
+  expect_px_px_save_preserves_everything(px_from_table_name("BEXSTA"))
+  expect_px_px_save_preserves_everything(px_from_table_name("FOTEST"))
+  expect_px_px_save_preserves_everything(px_from_table_name("no_timeval_or_codes"))
+  expect_px_px_save_preserves_everything(px_from_table_name("zero_heading"))
+  expect_px_px_save_preserves_everything(px_from_table_name("zero_stub"))
+
+
+  # Cell values with closing parenthesis )
+  population_gl |>
+    dplyr::mutate(across(age, ~ dplyr::case_when(. == "0-6"  ~ "(0-6)",
+                                                 . == "7-16"  ~ "(7)-16",
+                                                 . == "17-24" ~ "17-(24)",
+                                                 . == "25-64" ~ "25)-64",
+                                                 . =="65+"    ~ "65(",
+                                                 TRUE ~ .
+                                                 )
+                         )
+                  ) %>%
+    px() %>%
+    px_precision(dplyr::tribble(~`variable-code`, ~precision,
+                                'age', 3)
+                 ) %>%
+    px_cellnote(
+      dplyr::tribble(~gender,   ~age,  ~year, ~cellnote,
+                     'female',    '*', '2014', 'Uncertainty in ages',
+                     NA, "(0-6)", NA, 'Note to value with parensthsis ()'
+                     )) %>%
+    expect_px_px_save_preserves_everything()
 })

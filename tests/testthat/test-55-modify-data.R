333b10c2d0354a63a942aa82bb0c06aea8d0b317
Johan Ejstrud <johan@ejstrud.com>
2025-03-21 10:13:23 -0200

    Improve metadata updating by px_data
    Close #396

--- tests/testthat/test-55-modify-data.R
+++ tests/testthat/test-55-modify-data.R
@@ -19,14 +19,17 @@ test_that('data is modified', {
 })
 
 test_that('modifying data updates metadata', {
+  # Use a new data set where one variable changes name and one changes levels.
   population_gl_new <-
     population_gl %>%
-    dplyr::bind_rows(dplyr::tibble(gender='total',
-                                   age='0-6',
-                                   year = '2004',
-                                   n = 1
-                                   )
-                     ) %>%
+    dplyr::mutate(age = dplyr::if_else(age %in% c('0-6', '7-16'),
+                                       '0-16',
+                                       age
+                                       )
+                   ) %>%
+    dplyr::group_by(gender, age, year) %>%
+    dplyr::summarise(n = sum(n), .groups = "keep") %>%
+    dplyr::ungroup() %>%
     dplyr::mutate(year = stringr::str_sub(year, 3, 4)) %>%
     dplyr::rename(shortyear = year)
 
@@ -38,6 +41,8 @@ test_that('modifying data updates metadata', {
     x1 %>%
     px_data(population_gl_new)
 
+
+  ## Variables
   old_names <- names(population_gl)
   new_names <- names(population_gl_new)
   new_names_stub_heading <- setdiff(new_names, 'n')
@@ -60,4 +65,57 @@ test_that('modifying data updates metadata', {
   expect_identical(x2$acrosscell,
                    get_base_acrosscells(c(px_stub(x2), px_heading(x2)))
                    )
+
+  ## Codes
+  data_set_codes <- function(df) {
+    df %>%
+      dplyr::select(-n) %>%
+      tidyr::pivot_longer(everything(),
+                          names_to = "variable-code",
+                          values_to = 'code'
+                          ) %>%
+      dplyr::distinct_all()
+  }
+
+  old_codes <- data_set_codes(population_gl)
+
+  df_codes <- data_set_codes(population_gl_new)
+
+  removed_codes <-
+    old_codes  %>%
+    dplyr::anti_join(df_codes, by = dplyr::join_by(`variable-code`, code))
+
+  new_codes <-
+    df_codes %>%
+    dplyr::anti_join(old_codes, by = dplyr::join_by(`variable-code`, code))
+
+  dplyr::semi_join(removed_codes, x2$cells1,
+                   by = dplyr::join_by(`variable-code`, code)
+                   ) %>%
+    nrow() %>%
+    expect_equal(0)
+
+  expect_no_matches <- function(df1, df2) {
+    dplyr::semi_join(df1, df2,
+                     by = dplyr::join_by(`variable-code`, code)
+                     ) %>%
+      nrow() %>%
+      expect_equal(0)
+  }
+
+  expect_no_matches(removed_codes, x2$cells1)
+  expect_no_matches(removed_codes, x2$cells2)
+
+  expect_all_matches <- function(df1, df2) {
+    dplyr::anti_join(df1, df2,
+                     by = dplyr::join_by(`variable-code`, code)
+                     ) %>%
+      nrow() %>%
+      expect_equal(0)
+  }
+
+  expect_all_matches(new_codes, x2$cells1)
+  expect_all_matches(new_codes, x2$cells2)
 })
+
+

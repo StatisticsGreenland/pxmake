9602a378a608d5a92360505b46fe97294443241e
Johan Ejstrud <johan@ejstrud.com>
2025-12-02 14:09:13 -0200

    Add MAP in variables2 instead of table2
    Close #409

--- data-raw/px_keywords.R
+++ data-raw/px_keywords.R
@@ -4,10 +4,10 @@ px_keywords <-
   readr::read_tsv(file.path('data-raw', 'keywords.tsv'),
                   na = "NA",
                   col_types = "clllllcc"
-                  ) %>%
+                  ) |>
   dplyr::mutate(order = dplyr::row_number(),
                 px_function = keyword_to_function(keyword)
-                ) %>%
+                ) |>
   dplyr::relocate(px_function, .after = "keyword")
 
 usethis::use_data(px_keywords, overwrite = TRUE)
